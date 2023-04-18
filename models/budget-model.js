e9c2b5bfdc322f59bfb6dd2c0f9532429b7dd631
SuperNinjaEv <evan0210@gmail.com>
2023-04-18 18:15:00 -0400

    Cleaned up model data structure

--- models/budget-model.js
+++ models/budget-model.js
@@ -1,10 +1,3 @@
-// Your server should incorporate at least one table that, at minimum, includes:
-// id - A unique number for each item
-// item_name- string - the name of the transaction (ie: income, savings, cat food, etc.)
-// amount -number - the amount of the transaction
-// date- string - the date should be a simple string. As a bonus activity, use the date object and date input field and format it to be human-readable
-// from - string - who this transaction was with (ie. employer, bank, pet store, grocery store, etc)
-// category - string - what category does this fall into (income, savings, pets, food, etc)
 
 module.exports = [
     {
